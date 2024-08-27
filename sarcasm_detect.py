1f36d60972774d88256a42111f300b04a6b32db8
Muhammad40091 <129478070+Muhammad40091@users.noreply.github.com>
2024-08-27 23:01:06 +0500

    Add files via upload

--- sarcasm_detect.py
+++ sarcasm_detect.py
@@ -12,11 +12,31 @@ model = joblib.load('sarcasm_detect.joblib')
 # Define the vectorizer used in the model
 vectorizer = model.named_steps['vect']
 
-text = st.text_input("Enter Text: ", placeholder='Write a text for detect sarcasm or not sarcasm')
+# Define a list of sarcastic sentences for placeholder
+sarcastic_sentences = [
+    
+    "Deli Worker Searches For Palest, Mealiest Tomato To Put On Customer’s Sandwich",
+    "Cancer Researchers Develop Highly Promising New Pink Consumer Item",
+    "Pope Francis Renounces Papacy After Falling In Love With Beautiful American Divorcee",
+    "Insufferable Man Utters Words ‘Craft Beer Movement’",
+    "Anaheim Police Chief John Welter: 'Look, Our Job Is To Shoot People'",
+    "Single, Unemployed Mother Leeching Off Government",
+    "Mayor Of Phoenix Apologizes For Naming Berlin Germany Of 1941 As Sister City",
+    "Report: Average American Feels Comfortable In Own Skin For Only 6% Of Day",
+    "Report: You Were Lonely Before The Pandemic Started, And You’ll Be Lonely After It Ends",
+    "Zoologists: Ape Neurology Much Like That Of Banana-Obsessed Humans",
+    "Breaking: You Have Reached Your Free Article Limit",
+    "Nation Could Really Use A Few Days Where It Isn’t Gripped By Something"
+]
 
-col2, col3 = st.columns(2)
+# Select a random sarcastic sentence for the placeholder
+placeholder_text = random.choice(sarcastic_sentences)
 
-def handle_input_text():
+# Create the text input with the random sarcastic sentence as placeholder
+text = st.text_input("Enter Text: ", placeholder=placeholder_text)
+
+# Button to detect sarcasm
+if st.button("Detect🔍"):
     if len(text) != 0:
         # Transform the input text to the format used by the model
         input_vectorized = vectorizer.transform([text])
@@ -25,10 +45,8 @@ def handle_input_text():
         # Convert probability to prediction
         prediction = model.named_steps['clf'].predict(input_vectorized)[0]
         if prediction == 'Sarcastic':
-            col3.write("Sarcastic")
+            st.write("Sarcastic")
         else:
-            col3.write("Not Sarcastic")
+            st.write("Not Sarcastic")
     else:
-        col3.write("")
-
-col2.button("Detect🔍", on_click=handle_input_text)
+        st.write("Please enter some text.")
